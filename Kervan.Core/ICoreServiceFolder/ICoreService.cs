026e76845c08285e379bc4a9d519c94ed1b05773
Numan Kaplan <numankaplan32@gmail.com>
2025-06-18 15:28:43 +0300

    Add files via upload

---
+++ Kervan.Core/ICoreServiceFolder/ICoreService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kervan.Core.CoreServiceFolder;
+
+namespace Kervan.Core.ICoreServiceFolder
+{
+   public  interface ICoreService <T> where T : CoreService
+    {
+        /*Senkron Metodlar*/
+
+        public bool Create(T entity);
+        public bool Update(T entity);
+        public bool Delete(T entity);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+        public bool Save();
+
+        /*Asenkron Metodlar*/
+
+        public Task<bool> CreateAsync(T entity);
+        public Task<bool> UpdateAsync(T entity);
+        public Task<bool> DeleteAsync(T entity);
+        public Task<T> GetByIdAsync(int id);
+        public Task<IEnumerable<T>> GetAllAsync();
+        
+
+        public Task<bool> SaveAsync();
+
+    }
+}
